62a7bd67c31e32edcd6f03d342277bff6d95a840
evans <evansnt9619@gmail.com>
2024-06-08 13:38:28 +0300

    configuring

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite3:///app.db')
     if DATABASE_URL and DATABASE_URL.startswith('postgres://'):
         DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql+psycopg2://')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
