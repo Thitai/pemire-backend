0dcc676deb6839394c2125d6091f4ed1c6b068cc
evans <evansnt9619@gmail.com>
2024-06-08 13:28:09 +0300

    json

--- migrations/versions/c51408f12b7a_added_images.py
+++ migrations/versions/c51408f12b7a_added_images.py
@@ -25,8 +25,7 @@ def upgrade():
     sa.ForeignKeyConstraint(['motor_id'], ['motors.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-    with op.batch_alter_table('motors', schema=None) as batch_op:
-        batch_op.drop_column('images')
+    op.execute("ALTER TABLE motors ALTER COLUMN images TYPE JSON USING images::json")
 
     # ### end Alembic commands ###
 
